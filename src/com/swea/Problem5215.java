85850dcbc5179060b174dda4564ae5991ae64443
김효린 <hyorin97@naver.com>
2021-02-09 16:39:17 +0900

    refactor: Add pruning to swea 5215

--- src/com/swea/Problem5215.java
+++ src/com/swea/Problem5215.java
@@ -3,7 +3,6 @@ package com.swea;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
@@ -12,12 +11,12 @@ import java.util.StringTokenizer;
  * @problem SWEA 5215번 햄버거 다이어트
  * @see https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWT-lPB6dHUDFAVT&categoryId=AWT-lPB6dHUDFAVT&categoryType=CODE&problemTitle=5215&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=&pageSize=10&pageIndex=1
  * @mem 21,208
- * @time 784
+ * @time 784 -> 가지치기 후 706 (별 효과 X)
  * @caution 부분집합 - 재귀
  */
 public class Problem5215 {
 	static int L;
-	static int[][] kal;
+	static int[][] ingredient;
 	static int result = 0;
 
 	public static void main(String[] args) throws IOException {
@@ -32,15 +31,15 @@ public class Problem5215 {
 			int N = Integer.parseInt(st.nextToken());
 			L = Integer.parseInt(st.nextToken());
 
-			kal = new int[N][2];
+			ingredient = new int[N][2];
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
-				kal[i][0] = Integer.parseInt(st.nextToken());
-				kal[i][1] = Integer.parseInt(st.nextToken());
+				ingredient[i][0] = Integer.parseInt(st.nextToken());
+				ingredient[i][1] = Integer.parseInt(st.nextToken());
 			}
 
 			result = 0;
-			powerset(N, new boolean[N]);
+			powerset(N, new boolean[N], 0);
 
 			// print
 			sb.append("#").append(t).append(" ").append(result).append("\n");
@@ -49,30 +48,29 @@ public class Problem5215 {
 		System.out.println(sb);
 	}
 
-	private static void powerset(int toChoose, boolean[] chosen) {
+	private static void powerset(int toChoose, boolean[] chosen, int kal) {
+		if (kal > L) {
+			return;
+		}
 		if (toChoose == 0) {
 			result = Math.max(result, getSum(chosen));
 			return;
 		}
 
-		chosen[chosen.length - toChoose] = true;
-		powerset(toChoose - 1, chosen);
 		chosen[chosen.length - toChoose] = false;
-		powerset(toChoose - 1, chosen);
+		powerset(toChoose - 1, chosen, kal);
+
+		chosen[chosen.length - toChoose] = true;
+		kal += ingredient[chosen.length - toChoose][1];
+		powerset(toChoose - 1, chosen, kal);
 
 	}
 
 	private static int getSum(boolean[] chosen) {
-		int kalorie = 0;
 		int taste = 0;
 		for (int i = 0; i < chosen.length; i++) {
 			if (chosen[i]) {
-				taste += kal[i][0];
-				kalorie += kal[i][1];
-			}
-
-			if (kalorie > L) {
-				return 0;
+				taste += ingredient[i][0];
 			}
 		}
 
